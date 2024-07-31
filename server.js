998958e149b6aad2fd5f31e494603df3ddad7b4c
Sean Hyde <sean.hyde04@gmail.com>
2024-07-31 16:02:38 -0400

    login Success displays user data, styled,

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const bcrypt = require("bcryptjs");
 const port = 3019;
+const saltrounds = 10;
 
 const app = express();
 app.use(express.static(__dirname));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/users', {
+mongoose.connect("mongodb://127.0.0.1:27017/users", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
-db.once('open', () => {
-  console.log('MongoDB connection successful');
+db.once("open", () => {
+  console.log("MongoDB connection successful");
 });
 
 const userSchema = new mongoose.Schema({
@@ -22,34 +25,70 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-const User = mongoose.model('User', userSchema); // Use consistent case
+const User = mongoose.model("User", userSchema);
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.post('/post', async (req, res) => {
+app.post("/post", async (req, res) => {
   const { email, password } = req.body;
-  const user = new User({
-    email,
-    password,
-  });
-  await user.save();
-  console.log(user);
-  res.send("Form Submitted");
-});
 
-app.post('/login', async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email, password });
-    if (user) {
-        res.sendFile(path.join(__dirname, 'loginSuccess.html'));
-        console.log("User that signed in: ", user);
-    } else {
-        res.send('Login Failed');
+  bcrypt.genSalt(saltrounds, function (err, salt) {
+    if (err) {
+      console.error("ERROR: ", err);
+      return res.status(500).send("ERROR GENERATING SALT");
     }
-}
-);
+
+    bcrypt.hash(password, salt, function (err, hash) {
+      if (err) {
+        console.error("ERROR: ", err);
+        return res.status(500).send("ERROR GENERATING HASH");
+      }
+      // Store hash in database
+      console.log("Here is the hash:" + hash);
+
+      const user = new User({
+        email,
+        password: hash, // Storing the hash in the database
+      });
+
+      user
+        .save()
+        .then(() => {
+          console.log("User saved successfully");
+          console.log("User: ", user);
+          res.send("Form successfully submitted");
+        })
+        .catch((err) => {
+          console.error("ERROR: ", err);
+          res.status(500).send("ERROR SAVING USER");
+        });
+    });
+  });
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user) {
+    const isValid = await bcrypt.compare(password, user.password);
+    if (isValid) {
+      const timestamp = new Date().toString();
+      return res.json({
+        email: user.email,
+        password: user.password,
+        id: user._id,
+        timestamp: timestamp,
+      });
+    } else {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+  } else {
+    return res.status(401).json({ message: "User not found" });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
